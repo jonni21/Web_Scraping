35a56e7a964a04196c03bb0981521958684ca975
jonni21 <jhnzvzd@gmail.com>
2017-02-28 11:45:16 +0200

    glozis.py and requirements.txt adde

---
+++ glozis.py
@@ -0,0 +1,14 @@
+import urllib
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
+URL = 'https://www.etsy.com/ru/shop/GlozisDecor/items'
+ACCEPT_LANGUAGE = 'en=gb'
+
+ua = UserAgent()
+header = {'user-agent': ua.chrome, 'Accept-Language': ACCEPT_LANGUAGE}
+
+response = requests.get(URL, headers=header)
+soup = BeautifulSoup(response.content, 'lxml')
+print(soup.prettify())
